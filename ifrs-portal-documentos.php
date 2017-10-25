57cf33ea67393d9243259e4e03c1fd389b607700
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-10-25 14:12:50 -0200

    Adição de roles específicos do plugin.

--- ifrs-portal-documentos.php
+++ ifrs-portal-documentos.php
@@ -17,3 +17,4 @@ require_once('taxonomy-single-term/class.taxonomy-single-term.php');
 require_once('documento-type.php');
 require_once('documento-origin.php');
 require_once('documento.php');
+require_once('roles.php');
---
+++ roles.php
@@ -0,0 +1,52 @@
+<?php
+// Fix Media Permissions
+add_action('init', function() {
+    global $wp_post_types;
+    $wp_post_types['attachment']->cap->edit_posts = 'edit_files';
+    $wp_post_types['attachment']->cap->delete_posts = 'delete_files';
+});
+
+register_activation_hook(__FILE__, function () {
+    if (!get_role('cadastrador_documentos')) {
+        add_role('cadastrador_documentos', __('Cadastrador de Documentos'), array(
+            'read'                    => true,
+            'upload_files'            => true,
+            'edit_files'              => true,
+            'delete_files'            => false,
+
+            'create_documentos'       => true,
+            'publish_documentos'      => true,
+            'edit_documentos'         => true,
+            'delete_documentos'       => false,
+
+            'assign_documento_type'   => true,
+            'assign_documento_origin' => true
+        ));
+    }
+
+    if (!get_role('gerente_documentos')) {
+        add_role('gerente_documentos', __('Gerente de Documentos'), array(
+            'read'                    => true,
+            'upload_files'            => true,
+            'edit_files'              => true,
+            'delete_files'            => true,
+
+            'create_documentos'       => true,
+            'publish_documentos'      => true,
+            'edit_documentos'         => true,
+            'delete_documentos'       => true,
+
+            'assign_documento_type'   => true,
+            'assign_documento_origin' => true
+        ));
+    }
+});
+
+register_deactivation_hook(__FILE__, function () {
+    if (get_role('cadastrador_documentos')) {
+        remove_role('cadastrador_documentos');
+    }
+    if (get_role('gerente_documentos')) {
+        remove_role('gerente_documentos');
+    }
+});
