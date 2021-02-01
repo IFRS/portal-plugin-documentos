c46aecb746e04bdb6a43a802469bab4e1ad6faf7
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-02-01 11:02:17 -0300

    Reformulação dos filtros

--- templates/partials/archive.php
+++ templates/partials/archive.php
@@ -1,40 +1,39 @@
 <section class="documentos">
-    <div class="documentos__main">
-        <h2 class="documentos__title">
-            <?php
-            if (is_post_type_archive('documento')) {
-                _e('Documentos', 'ifrs-portal-plugin-documentos');
-            }
+    <h2 class="documentos__title">
+        <?php
+        if (is_post_type_archive('documento')) {
+            _e('Documentos', 'ifrs-portal-plugin-documentos');
+        }
 
-            if (is_tax('documento_type') && !isset($_POST['documento_type'])) {
-                single_term_title();
-            }
+        if (is_tax('documento_type') && !isset($_POST['documento_type'])) {
+            single_term_title();
+        }
 
-            if (is_tax('documento_origin') && !isset($_POST['documento_origin'])) {
-                printf(__('Documentos de %s', 'ifrs-portal-plugin-documentos'), single_term_title('', false));
-            }
+        if (is_tax('documento_origin') && !isset($_POST['documento_origin'])) {
+            printf(__('Documentos de %s', 'ifrs-portal-plugin-documentos'), single_term_title('', false));
+        }
 
-            if (is_search() && get_search_query()) {
-                printf(__('<small>(Resultados com o termo &ldquo;%s&rdquo;)</small>', 'ifrs-portal-plugin-documentos'), get_search_query());
-            }
-            ?>
-        </h2>
-        <?php echo wpautop(get_option( 'ifrs_documentos_intro' )); ?>
-        <?php if (have_posts()) : ?>
-            <?php load_template(plugin_dir_path(__FILE__) . 'loop.php'); ?>
+        if (is_search() && get_search_query()) {
+            printf(__('<small>(Resultados com o termo &ldquo;%s&rdquo;)</small>', 'ifrs-portal-plugin-documentos'), get_search_query());
+        }
+        ?>
+    </h2>
+
+    <?php echo wpautop(get_option( 'ifrs_documentos_intro' )); ?>
+
+    <?php load_template(plugin_dir_path(__FILE__) . 'filter.php'); ?>
+
+    <?php if (have_posts()) : ?>
+        <?php load_template(plugin_dir_path(__FILE__) . 'loop.php'); ?>
+    <?php else : ?>
+        <?php if (is_search()) : ?>
+            <div class="alert alert-danger" role="alert">
+                <p><?php _e('N&atilde;o foram encontrados Documentos com os termos buscados.'); ?></p>
+            </div>
         <?php else : ?>
-            <?php if (is_search()) : ?>
-                <div class="alert alert-danger" role="alert">
-                    <p><?php _e('N&atilde;o foram encontrados Documentos com os termos buscados.'); ?></p>
-                </div>
-            <?php else : ?>
-                <div class="alert alert-warning" role="alert">
-                    <p><strong><?php _e('Ops!'); ?></strong>&nbsp;<?php printf(__('N&atilde;o foram encontrados Documentos publicados.', 'ifrs-portal-plugin-documentos'), single_term_title('', false)); ?></p>
-                </div>
-            <?php endif; ?>
+            <div class="alert alert-warning" role="alert">
+                <p><strong><?php _e('Ops!'); ?></strong>&nbsp;<?php printf(__('N&atilde;o foram encontrados Documentos publicados.', 'ifrs-portal-plugin-documentos'), single_term_title('', false)); ?></p>
+            </div>
         <?php endif; ?>
-    </div>
-    <div class="documentos__aside">
-        <?php load_template(plugin_dir_path(__FILE__) . 'filter.php'); ?>
-    </div>
+    <?php endif; ?>
 </section>
--- templates/partials/filter.php
+++ templates/partials/filter.php
@@ -10,37 +10,51 @@
         'hide_empty' => false,
         'orderby' => 'term_order',
     ));
+
+    $has_filter = isset($_POST['documento_type']) || isset($_POST['documento_origin']);
 ?>
 <aside class="filter">
-    <h3 class="filter__title"><?php _e('Filtros', 'ifrs-portal-plugin-documentos'); ?></h3>
+    <?php $collapse_id = uniqid(); ?>
+    <button
+        class="btn btn-link filter__btn<?php echo ($has_filter) ? ' font-weight-bold' : ''; ?>"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collpse-<?php echo $collapse_id; ?>"
+        aria-expanded="<?php echo ($has_filter) ? 'true' : 'false'; ?>"
+        aria-controls="collapse-<?php echo $collapse_id; ?>"
+    >
+        <?php _e('Filtros', 'ifrs-portal-plugin-documentos'); ?>
+    </button>
 
-    <form action="<?php echo get_post_type_archive_link( 'documento' ); ?>" method="POST" class="filter__form">
-        <fieldset>
-            <legend>Tipo</legend>
-            <?php foreach ($tipos as $tipo): ?>
-                <?php $field_id = uniqid(); ?>
-                <?php $tipo_check = (isset($_POST['documento_type']) && in_array($tipo->slug, $_POST['documento_type'])) || is_tax('documento_type', $tipo->slug); ?>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="documento_type[]" value="<?php echo $tipo->slug; ?>" id="<?php echo $field_id; ?>" <?php echo $tipo_check ? 'checked' : ''; ?>>
-                    <label class="form-check-label" for="<?php echo $field_id; ?>"><?php echo $tipo->name; ?></label>
-                </div>
-            <?php endforeach; ?>
-        </fieldset>
-        <fieldset>
-            <legend>Origem</legend>
-            <?php foreach ($origens as $origem): ?>
-                <?php $field_id = uniqid(); ?>
-                <?php $origem_check = (isset($_POST['documento_origin']) && in_array($origem->slug, $_POST['documento_origin'])) || is_tax('documento_origin', $origem->slug); ?>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="documento_origin[]" value="<?php echo $origem->slug; ?>" id="<?php echo $field_id; ?>" <?php echo $origem_check ? 'checked' : ''; ?>>
-                    <label class="form-check-label" for="<?php echo $field_id; ?>"><?php echo $origem->name; ?></label>
-                </div>
-            <?php endforeach; ?>
-        </fieldset>
+    <div class="collapse<?php echo ($has_filter) ? ' show' : ''; ?>" id="collpse-<?php echo $collapse_id; ?>">
+        <form action="<?php echo get_post_type_archive_link( 'documento' ); ?>" method="POST" class="filter__form">
+            <fieldset>
+                <legend>Tipo</legend>
+                <?php foreach ($tipos as $tipo): ?>
+                    <?php $field_id = uniqid(); ?>
+                    <?php $tipo_check = (isset($_POST['documento_type']) && in_array($tipo->slug, $_POST['documento_type'])) || is_tax('documento_type', $tipo->slug); ?>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="documento_type[]" value="<?php echo $tipo->slug; ?>" id="<?php echo $field_id; ?>" <?php echo $tipo_check ? 'checked' : ''; ?>>
+                        <label class="form-check-label" for="<?php echo $field_id; ?>"><?php echo $tipo->name; ?></label>
+                    </div>
+                <?php endforeach; ?>
+            </fieldset>
+            <fieldset>
+                <legend>Origem</legend>
+                <?php foreach ($origens as $origem): ?>
+                    <?php $field_id = uniqid(); ?>
+                    <?php $origem_check = (isset($_POST['documento_origin']) && in_array($origem->slug, $_POST['documento_origin'])) || is_tax('documento_origin', $origem->slug); ?>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="documento_origin[]" value="<?php echo $origem->slug; ?>" id="<?php echo $field_id; ?>" <?php echo $origem_check ? 'checked' : ''; ?>>
+                        <label class="form-check-label" for="<?php echo $field_id; ?>"><?php echo $origem->name; ?></label>
+                    </div>
+                <?php endforeach; ?>
+            </fieldset>
 
-        <div class="btn-group" role="group" aria-label="Ações do Filtro">
-            <input type="submit" value="Filtrar" class="btn btn-primary">
-            <a href="<?php echo get_post_type_archive_link( 'documento' ); ?>" class="btn btn-outline-secondary"><?php _e('Limpar', 'ifrs-portal-plugin-documentos'); ?></a>
-        </div>
-    </form>
+            <div class="btn-group" role="group" aria-label="Ações do Filtro">
+                <input type="submit" value="Filtrar" class="btn btn-primary">
+                <a href="<?php echo get_post_type_archive_link( 'documento' ); ?>" class="btn btn-outline-secondary"><?php _e('Limpar', 'ifrs-portal-plugin-documentos'); ?></a>
+            </div>
+        </form>
+    </div>
 </aside>
